01da78c28c9585250e651cc4cdb48acc84f3a2e2
LAPTOP-DA9KAPOP\gefenduo <gefenduo@mail.com>
2024-04-29 15:11:26 +0800

    remove files

--- static/js/dynamic_msg.js
+++
@@ -1,8 +0,0 @@
-function getBottomNavMsg() {
-    var isIOS2 = navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-    if (isIOS2) {
-        document.writeln('<a class="downnow downIos" href="javascript:;">立即下载</a>');
-    } else {
-        document.writeln('<a class="downnow downAndroid" href="javascript:;">立即下载</a>');
-    }
-}
